16f13f6f834c76fcb1e4912e63b88bb219a9bc2c
Чиркова Дарья <dasha.chirkova.05@inbox.ru>
2025-09-23 09:00:46 +0300

    Initial Commit

---
+++ Chirkova-Daria/Chirkova_DariaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chirkova_DariaApp.swift
+//  Chirkova-Daria
+//
+//  Created by Даша Чиркова on 23.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Chirkova_DariaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
